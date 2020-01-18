1faf867e641fadf18e48b8f3eeff75e7b0580351
Sergejs Basangovs <sergejs.basangovs@ingresso.co.uk>
2020-01-18 19:50:58 +0000

    optimised, folder structure improved

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,46 @@
-import React from "react";
-import Aux from "../../../hoc/Aux";
+import React, { Component } from "react";
+import Aux from "../../../hoc/Aux/Aux";
 import Button from "../../UI/Button/Button";
 
-const OrderSummary = props => {
-	const ingredients = Object.keys(props.ingredients).map(ingName => {
-		return (
-			<li key={ingName}>
-				<span style={{ textTransform: "capitalize" }}>{ingName}</span>:{" "}
-				{props.ingredients[ingName]}
-			</li>
-		);
-	});
+class OrderSummary extends Component {
+	//This could be a functional component.
 
-	return (
-		<Aux>
-			<h3>Your Order.</h3>
-			<p>A delicious burger with the following ingredients:</p>
-			<ul>{ingredients}</ul>
-			<p>
-				<strong>Total price: ${props.price.toFixed(2)}</strong>
-			</p>
-			<p>Continue to checkout?</p>
-			<Button clicked={props.purchaseCanceled} buttonType="Danger">
-				CANCEL
-			</Button>
-			<Button clicked={props.purchaseContinued} buttonType="Success">
-				CONTINUE
-			</Button>
-		</Aux>
-	);
-};
+	render() {
+		const ingredients = Object.keys(this.props.ingredients).map(ingName => {
+			return (
+				<li key={ingName}>
+					<span style={{ textTransform: "capitalize" }}>
+						{ingName}
+					</span>
+					: {this.props.ingredients[ingName]}
+				</li>
+			);
+		});
+
+		return (
+			<Aux>
+				<h3>Your Order.</h3>
+				<p>A delicious burger with the following ingredients:</p>
+				<ul>{ingredients}</ul>
+				<p>
+					<strong>Total price: ${this.props.price.toFixed(2)}</strong>
+				</p>
+				<p>Continue to checkout?</p>
+				<Button
+					clicked={this.props.purchaseCanceled}
+					buttonType="Danger"
+				>
+					CANCEL
+				</Button>
+				<Button
+					clicked={this.props.purchaseContinued}
+					buttonType="Success"
+				>
+					CONTINUE
+				</Button>
+			</Aux>
+		);
+	}
+}
 
 export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,36 @@
-import React from "react";
-import Aux from "../../../hoc/Aux";
+import React, { Component } from "react";
+import Aux from "../../../hoc/Aux/Aux";
 import Backdrop from "../Backdrop/Backdrop";
 import classes from "./Modal.module.css";
 
-const Modal = props => {
-	return (
-		<Aux>
-			<div
-				className={classes.Modal}
-				style={{
-					transform: props.show
-						? "translateY(0)"
-						: "translateY(-100vh)",
-					opacity: props.show ? "1" : "0"
-				}}
-			>
-				{props.children}
-			</div>
-			<Backdrop show={props.show} click={props.modalClosed} />
-		</Aux>
-	);
-};
+class Modal extends Component {
+	//This could be a functional component.
+
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.show !== this.props.show;
+	}
+
+	render() {
+		return (
+			<Aux>
+				<div
+					className={classes.Modal}
+					style={{
+						transform: this.props.show
+							? "translateY(0)"
+							: "translateY(-100vh)",
+						opacity: this.props.show ? "1" : "0"
+					}}
+				>
+					{this.props.children}
+				</div>
+				<Backdrop
+					show={this.props.show}
+					click={this.props.modalClosed}
+				/>
+			</Aux>
+		);
+	}
+}
 
 export default Modal;

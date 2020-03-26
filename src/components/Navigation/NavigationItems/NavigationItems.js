66ef63e388524be609a2606f5991a3f4e454c7f2
Sergejs Basangovs <sergejs.basangovs@ingresso.co.uk>
2020-03-26 20:43:14 +0000

    authenticated conditional ui element render

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,21 +3,21 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 import classes from "./NavigationItems.module.css";
 
 const NavigationItems = props => {
-	let authentication = (
-		<NavigationItem link="/auth">Authenticate</NavigationItem>
-	);
-
-	if (props.isAuthenticated) {
-		authentication = <NavigationItem link="/logout">Logout</NavigationItem>;
-	}
-
 	return (
 		<ul className={classes.NavigationItems}>
 			<NavigationItem link="/" exact>
 				Burger Builder
 			</NavigationItem>
-			<NavigationItem link="/orders">Orders</NavigationItem>
-			{authentication}
+
+			{props.isAuthenticated ? (
+				<NavigationItem link="/orders">Orders</NavigationItem>
+			) : null}
+
+			{!props.isAuthenticated ? (
+				<NavigationItem link="/auth">Authenticate</NavigationItem>
+			) : (
+				<NavigationItem link="/logout">Logout</NavigationItem>
+			)}
 		</ul>
 	);
 };

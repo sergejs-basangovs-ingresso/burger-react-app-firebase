3d71dc1a933e49a9270ac5262330e04592422dd0
Sergejs Basangovs <sergejs.basangovs@ingresso.co.uk>
2020-03-29 17:50:29 +0100

    section 19 optimization completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import Auth from "./containers/Auth/Auth";
-import Logout from "./containers/Auth/Logout/Logout";
+
 import * as actions from "./store/actions/index";
+import asyncComponent from "./hoc/asyncComponent/asyncComponent";
+
+//Lazy loading components:
+const asyncCheckout = asyncComponent(() =>
+	import("./containers/Checkout/Checkout")
+);
+const asyncOrders = asyncComponent(() => import("./containers/Orders/Orders"));
+const asyncAuth = asyncComponent(() => import("./containers/Auth/Auth"));
+const asyncLogout = asyncComponent(() =>
+	import("./containers/Auth/Logout/Logout")
+);
 
 class App extends Component {
 	componentDidMount() {
@@ -18,7 +26,7 @@ class App extends Component {
 		let routes = (
 			<Switch>
 				<Route path="/" exact component={BurgerBuilder} />
-				<Route path="/auth" component={Auth} />
+				<Route path="/auth" component={asyncAuth} />
 				<Redirect to="/" />
 			</Switch>
 		);
@@ -27,10 +35,10 @@ class App extends Component {
 			routes = (
 				<Switch>
 					<Route path="/" exact component={BurgerBuilder} />
-					<Route path="/checkout" component={Checkout} />
-					<Route path="/orders" component={Orders} />
-					<Route path="/logout" component={Logout} />
-					<Route path="/auth" component={Auth} />
+					<Route path="/checkout" component={asyncCheckout} />
+					<Route path="/orders" component={asyncOrders} />
+					<Route path="/logout" component={asyncLogout} />
+					<Route path="/auth" component={asyncAuth} />
 					<Redirect to="/" />
 				</Switch>
 			);
